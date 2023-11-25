5ea5be21c70885989f1835e872804e2fce5fd60a
joao <joaov.petricio2002@gmail.com>
2023-11-25 20:47:07 -0300

    add pokemons no html

--- site_pokemon/pokedex/assets/js/main.js
+++ site_pokemon/pokedex/assets/js/main.js
@@ -1,7 +1,36 @@
 const offset = 0;
-const limit = 0;
+const limit = 10;
 const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`
+
+
+function convertPokemonToLi(pokemon){
+    return `
+    <li class="pokemon">
+    <span class="number">${pokemon.number}</span>
+    <span class="name">${pokemon.name}</span>                
+
+    <div class="detail">
+        <ol class="types">
+            <li class="type">Grass</li>
+            <li class="type">Poison</li>
+        </ol>
+
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/5.svg" alt="${pokemon.name}">
+    </div>
+</li>
+    `
+
+}
+const pokemonOl = document.getElementById('pokemonList')
+
 
 fetch(url)
     .then((response) => response.json())
-    .then((jsonBody) => console.log())
+    .then((jsonBody) => jsonBody.results)
+    .then((pokemonList) => {
+        for (let i = 0; i < pokemonList.length; i++) {
+            const pokemon = pokemonList[i];
+            pokemonOl.innerHTML += convertPokemonToLi(pokemon)
+        }
+    })
+    .catch((error) => console.log(error))
